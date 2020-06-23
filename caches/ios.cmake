4497f09d33eee9df0e0324152dcf6d1d4cbe8f03
Felix Barz <felix.barz@brickmakers.de>
2020-06-23 08:27:40 +0200

    try stuff

---
+++ caches/ios.cmake
@@ -0,0 +1,2 @@
+set(HUNTER_URL "https://github.com/cpp-pm/hunter/archive/v0.23.259.tar.gz")
+set(HUNTER_SHA1 "59541baf106b91ae4fec32e6c6b0990d04c7f6be")
---
+++ caches/wasm_32.cmake
@@ -0,0 +1,3 @@
+set(CMAKE_TOOLCHAIN_FILE "$ENV{EMSDK}/fastcomp/emscripten/cmake/Modules/Platform/Emscripten.cmake" CACHE PATH "")
+#set(CMAKE_EXECUTABLE_SUFFIX ".wasm.js" CACHE STIRNG "")
+#LFLAGS "-s WASM=1 -s EXPORTED_FUNCTIONS='[_main]'"
