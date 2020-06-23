8efa0e389b3f89528d3106310c270ce0eedc4b83
Felix Barz <felix.barz@brickmakers.de>
2020-06-23 09:15:40 +0200

    try fixup wasm/clang

--- caches/wasm_32.cmake
+++ caches/wasm_32.cmake
@@ -1,3 +1,3 @@
-set(CMAKE_TOOLCHAIN_FILE "$ENV{EMSDK}/fastcomp/emscripten/cmake/Modules/Platform/Emscripten.cmake" CACHE PATH "")
-#set(CMAKE_EXECUTABLE_SUFFIX ".wasm.js" CACHE STIRNG "")
-#LFLAGS "-s WASM=1 -s EXPORTED_FUNCTIONS='[_main]'"
+set(CMAKE_TOOLCHAIN_FILE "$ENV{EMSDK}/upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake" CACHE PATH "")
+set(CMAKE_EXECUTABLE_SUFFIX ".wasm.js" CACHE STIRNG "")
+set(CMAKE_EXE_LINKER_FLAGS "-s WASM=1 -s EXPORTED_FUNCTIONS='[_main]' ${CMAKE_EXE_LINKER_FLAGS}" CACHE STIRNG "")
